b325fba72258ab0538f879a51e3728c43fe45856
Daniel BN <daniel.bn1800@gmail.com>
2024-09-15 10:46:01 -0700

    Update sms_api.vb

--- sms_api.vb
+++ sms_api.vb
@@ -1,3 +1,115 @@
+'vb.net
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports Newtonsoft.Json
+
+Module SmsApi
+
+    Sub Main()
+        ' Example usage
+        SendSms("256772123456", "Test SMS from True African")
+    End Sub
+
+    ' Function to send SMS
+    Sub SendSms(msisdn As String, message As String)
+        ' API Endpoint
+        Dim url As String = "http://mysms.trueafrican.com/v1/api/esme/send"
+
+        ' True African SMS credentials
+        Dim username As String = "your_username"
+        Dim password As String = "your_password"
+
+        ' Create JSON payload
+        Dim data As New Dictionary(Of String, Object) From {
+            {"msisdn", New List(Of String) From {msisdn}},
+            {"message", message},
+            {"username", username},
+            {"password", password}
+        }
+
+        Dim jsonData As String = JsonConvert.SerializeObject(data)
+
+        ' Path to the log file
+        Dim logFile As String = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sms_api.log")
+
+        ' Log the request start time
+        LogMessage(logFile, "Request Start Time: " & DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))
+        LogMessage(logFile, "Sending SMS to: " & msisdn & " with message: " & message)
+
+        ' Send the POST request
+        Try
+            Dim response As String = SendPostRequest(url, jsonData)
+
+            ' Log the API response
+            LogMessage(logFile, "API Response: " & response)
+
+            ' Decode the response
+            Dim responseData As Dictionary(Of String, Object) = JsonConvert.DeserializeObject(Of Dictionary(Of String, Object))(response)
+
+            ' Handle the response based on the code
+            If responseData.ContainsKey("code") Then
+                Dim responseCode As Integer = Convert.ToInt32(responseData("code"))
+
+                Select Case responseCode
+                    Case 200
+                        LogMessage(logFile, "Message sent successfully.")
+                    Case 204
+                        LogMessage(logFile, "Request error: Check your request parameters.")
+                    Case 209
+                        LogMessage(logFile, "Authentication error: Invalid username or password.")
+                    Case 207
+                        LogMessage(logFile, "Bulk account error: Please check your account settings.")
+                    Case Else
+                        LogMessage(logFile, "Unexpected error: " & response)
+                End Select
+            Else
+                LogMessage(logFile, "Unexpected error: " & response)
+            End If
+
+        Catch ex As Exception
+            ' Log any error
+            LogMessage(logFile, "Error: " & ex.Message)
+        End Try
+
+        ' Log the request end time
+        LogMessage(logFile, "Request End Time: " & DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))
+    End Sub
+
+    ' Function to send a POST request
+    Function SendPostRequest(url As String, jsonData As String) As String
+        Dim request As HttpWebRequest = CType(WebRequest.Create(url), HttpWebRequest)
+        request.Method = "POST"
+        request.ContentType = "application/json"
+
+        Dim data As Byte() = Encoding.UTF8.GetBytes(jsonData)
+        request.ContentLength = data.Length
+
+        Using stream = request.GetRequestStream()
+            stream.Write(data, 0, data.Length)
+        End Using
+
+        Dim responseString As String
+        Using response = CType(request.GetResponse(), HttpWebResponse)
+            Using reader = New StreamReader(response.GetResponseStream())
+                responseString = reader.ReadToEnd()
+            End Using
+        End Using
+
+        Return responseString
+    End Function
+
+    ' Function to log messages to a file
+    Sub LogMessage(filePath As String, message As String)
+        Dim logMessage As String = "[" & DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") & "] " & message & Environment.NewLine
+        File.AppendAllText(filePath, logMessage)
+    End Sub
+
+End Module
+
+
+'vba-------------------------------
+
 Sub sendSms(msisdn As String, message As String)
     ' API Endpoint
     Dim url As String
